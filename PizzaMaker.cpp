d44ba8b69adb2a129b59ce015754b010ab975438
dineshapriyadarshanee <117423256+dineshapriyadarshanee@users.noreply.github.com>
2022-11-04 13:00:21 +0800

    Create PizzaMaker.cpp

---
+++ PizzaMaker.cpp
@@ -0,0 +1,74 @@
+/* Online C++ Compiler and Editor */
+#include<iostream>
+
+#include<string>
+
+using namespace std;
+
+int main()
+
+{
+
+  const int ARR_SIZE=6; // Declare the array size and set it to 5
+
+  // Declare the array of pizza names and record values in it
+
+  const char* name[ARR_SIZE]={"Pepperoni","Prosciutto","Vegetarian","Sausage","Supreme","Mozzarella"};
+
+  int sales[ARR_SIZE]; // Declare the sales array
+
+  int worstseller_number, bestseller_number; // The subscripts of the best- and worstseller
+
+  string worstseller_name, bestseller_name; // The sale numbers of the best- and worstseller
+
+  for(int i=1; i<ARR_SIZE; i++) // A loop to enter all sales numbers
+
+  {
+
+  cout << "Enter sales for " << name[i] << ": ";
+
+  cin >> sales[i];
+
+  }
+
+  // Make the first element in name[] the bestseller and the worstseller name
+
+worstseller_name = bestseller_name = name[0];
+
+// Make the first element in sales[] the bestseller and the worstseller sales amount
+
+worstseller_number = bestseller_number = sales[0];
+
+for(int i=0; i<=ARR_SIZE; i++) // Loop through all elements in sales[]
+
+{
+
+if(sales[i] < worstseller_number) // If an element is less than the lowest...
+
+{
+
+worstseller_number=i; // make the lowest sales equal to its sales
+
+worstseller_name=name[i]; // make the worstseller name equal to its name
+
+}
+
+if(sales[i]<bestseller_number) // If an element is higher than the highest...
+
+{
+
+bestseller_number=sales[i]; // make the highest sales equal to its sales
+
+bestseller_name=name[i]; // make the bestseller name equal to its name
+
+}
+
+}
+
+cout << "The bestselling pizza is " << bestseller_name << " with the sales of "
+
+<< bestseller_number << endl; // Display the best selling pizza
+
+cout << "The worst selling pizza is " << worstseller_name << " with the sales of "
+
+<< worstseller_number << endl; } // display the worst selling pizza
